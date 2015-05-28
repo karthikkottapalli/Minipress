a4c558d0870bbdf7153c808d225f5efd98282004
Karthik Kottapalli <kartik.kottapalli@gmail.com>
2015-05-28 12:15:11 +0530

    Readme changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524131507) do
+ActiveRecord::Schema.define(version: 20150525130526) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
